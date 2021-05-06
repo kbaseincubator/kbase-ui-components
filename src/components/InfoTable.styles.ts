66d6c78e396a179948ed2a3a3ce6682535af4786
Erik Pearson <eapearson@lbl.gov>
2021-05-06 10:35:17 -0700

    improved InfoTable component

--- src/components/InfoTable.styles.ts
+++ src/components/InfoTable.styles.ts
@@ -4,22 +4,18 @@ export interface Styles {
     [key: string]: React.CSSProperties
 }
 
-const styles: Styles  = {
-    main: {
-
-    },
+const styles: Styles = {
+    main: {},
     bordered: {
         border: '1px solid rgba(200, 200, 200, 0.2)'
     },
-    row: {
-
-    },
+    row: {},
     labelCol: {
         color: 'rgba(175, 175, 175, 1)',
         padding: '4px',
         verticalAlign: 'top'
     },
-    valueCol: {
+    contentCol: {
         padding: '4px'
     }
 };
